669fc8c55c40db3fc2a55f53b95692bc8ee7644c
Dave Revell <revell@google.com>
2023-03-27 16:22:47 -0700

    WIP migration from skaffold to goreleaser

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,22 @@
+# Use distroless for ca certs.
+FROM gcr.io/distroless/static AS distroless
+
+# Use a scratch image to host our binary.
 FROM scratch
-ENTRYPOINT ["/mybin"]
-COPY mybin /
+
+ARG APPx
+
+COPY --from=distroless /etc/passwd /etc/passwd
+COPY --from=distroless /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+
+COPY $APP /server
+
+# Normally we would set this to run as "nobody".
+# But goreleaser builds the binary locally and sometimes it will mess up the permission
+# and cause "exec user process caused: permission denied".
+#
+# USER nobody
+
+# Run the web service on container startup.
+ENV PORT 8080
+ENTRYPOINT ["/server"]
--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -1,32 +1,31 @@
-
 // A minimal Cloud Run service.
 package main
 
 import (
-        "fmt"
-        "log"
-        "net/http"
-        "os"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-        log.Print("starting server...")
-        http.HandleFunc("/", handler)
+	log.Print("starting server...")
+	http.HandleFunc("/", handler)
 
-        // Determine port for HTTP service.
-        port := os.Getenv("PORT")
-        if port == "" {
-                port = "8080"
-                log.Printf("defaulting to port %s", port)
-        }
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
 
-        // Start HTTP server.
-        log.Printf("listening on port %s", port)
-        if err := http.ListenAndServe(":"+port, nil); err != nil {
-                log.Fatal(err)
-        }
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Bets platform, best platform. (003)\n")
+	fmt.Fprintf(w, "Bets platform, best platform. (004)\n")
 }
